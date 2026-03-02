7a69e6124d2eda095dafce2623abf499c2b84cc1
dv3882-jpg <dv3882@srmist.edu.in>
2026-03-02 21:52:24 +0530

    console UC6 to print OOPS to console

--- OOPSBannerAppUC6.java
+++ OOPSBannerAppUC6.java
@@ -1,52 +1,68 @@
-public static String getOPattern(){
-    return new String[] {
-	    "   ***   ",
-		" **   ** ",
-		"**     **",
-		"**     **",
-		"**     **",
-		"**     **",
-		"**     **",
-		" **   ** ",
-		"   ***   ",
-	};
+public class OOPSBannerAppUC6{
+    public static String[] getOPattern(){
+        return new String[] {
+	        "   ***   ",
+		    " **   ** ",
+		    "**     **",
+		    "**     **",
+		    "**     **",
+		    "**     **",
+		    "**     **",
+		    " **   ** ",
+		    "   ***   ",
+	    };
+    }
+
+    public static String[] getOOPattern() {
+        return new String[] {
+	        "   ***   ",
+     		" **   ** ",
+	    	"**     **",
+		    "**     **",
+		    "**     **",
+		    "**     **",
+		    "**     **",
+		    " **   ** ",
+		    "   ***   ",
+	    };
+    }
+    public static String[] getPPattern() {
+        return new String[] {
+	        "******   ",
+		    "**    ** ",
+		    "**     **",
+		    "**    ** ",
+		    "******   ",
+		    "**       ",
+		    "**       ",
+		    "**       ",
+		    "**       ",
+	    };
+    }
+    public static String[] getSPattern() {
+        return new String[] {
+	        "   ***** ",
+		    " **      ",
+		    "**       ",
+		    " **      ",
+		    "   ***   ",
+		    "      ** ",
+		    "       **",
+		    "      ** ",
+		    " *****   ",
+	    };
+    }
+	
+	public static void main(String[] args){
+		String[] OPattern = getOPattern();
+		String[] OOPattern = getOOPattern();
+		String[] PPattern = getPPattern();
+		String[] SPattern = getSPattern();
+
+        for ( int i =0;i <OPattern.length;i++){
+			System.out.println(OPattern[i] + " " + OOPattern[i] + " " + PPattern[i] + " " + SPattern[i]);
+        }
+	}
 }
-public static String getOOPattern() {
-    return new String[] {
-	    "   ***   ",
-		" **   ** ",
-		"**     **",
-		"**     **",
-		"**     **",
-		"**     **",
-		"**     **",
-		" **   ** ",
-		"   ***   ",
-	};
-}
-public static String getPPattern() {
-    return new String[] {
-	    "******   ",
-		"**    ** ",
-		"**     **",
-		"**    ** ",
-		"******   ",
-		"**       ",
-		"**       ",
-		"**       ",
-		"**       ",
-	};
-}
-public static String getPPattern() {
-    return new String[] {
-	    "   ***** ",
-		" **      ",
-		"**       ",
-		" **      ",
-		"   ***   ",
-		"      ** ",
-		"       **",
-		"      ** ",
-		" *****   ",
-	};
-}
+
+
